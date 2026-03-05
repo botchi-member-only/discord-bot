15d48af508d6634a68dcf81304b57a24e18e7788
botchi-member-only <botchi.member.only@gmail.com>
2026-03-05 12:09:25 +0900

    Update game_manager.py

--- game_manager.py
+++ game_manager.py
@@ -500,10 +500,9 @@ def setup(tree: app_commands.CommandTree):
 
         if result_channel:
             await result_channel.send(
-                uid = entry["user_id"]
                 f"🏁 **タイム提出**\n"
                 #f"👤 {user_name}\n"
-                f"text += f"　👤 <@{uid}>\n"
+                f"👤 <@{user_id}>\n"
                 f"🗺️ {course}\n"
                 f"⏱️ {time}"
             )
